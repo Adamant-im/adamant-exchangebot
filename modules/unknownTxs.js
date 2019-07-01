bbf04645314dda4f2b823126c5fb7d6f790f0de4
Bob Braun <bobbraunjedi@gmail.com>
2019-07-01 21:49:35 +0300

    add 24 hours count unknown txs

--- modules/unknownTxs.js
+++ modules/unknownTxs.js
@@ -12,7 +12,7 @@ module.exports = async (tx, itx) => {
 		}).sort({date: -1}).toArray((err, docs) => {
 			const twoHoursAgo = $u.unix() - 2 * 3600 * 1000;
 			let countMsgs = docs.length;
-			if (!docs.length || twoHoursAgo < docs[0].date){
+			if (!docs.length || twoHoursAgo < docs[1].date){
 				countMsgs = 1;
 			}
 
